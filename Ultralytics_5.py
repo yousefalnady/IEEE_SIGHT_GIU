4c9fd5a3b63ff381dddef2c9351082c157af351a
yousefalnady <yousefalnady@gmail.com>
2024-03-10 19:09:40 +0200

    most recent updated pushing to a new branch

---
+++ Ultralytics_5.py
@@ -0,0 +1,75 @@
+import cv2
+from ultralytics import YOLO
+from gtts import gTTS
+from io import BytesIO
+import pygame
+from googletrans import Translator
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def capture_image():
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return
+
+    ret, frame = cap.read()
+
+    if ret:
+        cv2.imwrite('captured_image.jpg', frame)
+        print("Image captured successfully.")
+    else:
+        print("Error: Could not capture image.")
+
+    cap.release()
+
+
+print('Waiting for button press...')
+while True:
+    if GPIO.input(4) == GPIO.HIGH:
+        print("Button was pushed!")
+        break
+print('Button pressed.')
+
+capture_image()
+
+model = YOLO('yolov8n.pt')
+pygame.init()
+pygame.mixer.init()
+translator = Translator(service_urls=['translate.googleapis.com'])
+
+results = model("captured_image.jpg", conf=0.4, show=True)
+
+names = model.names
+
+for r in results:
+    for c in r.boxes.cls:
+        class_name = names[int(c)]
+        print(class_name)
+
+        # Translate the class name to Arabic
+        translation = translator.translate(class_name, dest='ar')
+        translated_text = translation.text
+        print(translated_text)
+
+        # Convert translated text to audio and play
+        mp3_fp = BytesIO()
+        tts = gTTS(translated_text, lang='ar')
+        tts.save('translated_audio.mp3')
+        tts.write_to_fp(mp3_fp)
+        mp3_fp.seek(0)
+        
+        # Play the translated audio
+        sound = pygame.mixer.Sound(mp3_fp)
+        playing = sound.play()
+        while playing.get_busy():
+             pygame.time.delay(100)
+        time.sleep(0.5)
+        mp3_fp.close()
--- gpio_button.py
+++ gpio_button.py
@@ -1,4 +1,8 @@
-from gpiozero import Button
-button = Button(7)
-button.wait_for_press()
-print('You pushed me')
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+while True: # Run forever
+    if GPIO.input(4) == GPIO.HIGH:
+        print("Button was pushed!")
